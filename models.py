67ed3f0fc37fe2bfc4884a148c28770c2a1ab509
Дроздов Иван Сергеевич <i.drozdov@main.nsu.ru>
2025-12-01 15:09:48 +0700

    Fix Code

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-# models.py - обновленная модель Equipment
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,39 +8,50 @@ class Equipment(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     ip_address = db.Column(db.String(15), nullable=False, unique=True, index=True)
-    hostname = db.Column(db.String(255))
     mac_address = db.Column(db.String(17))
-    vlan_id = db.Column(db.Integer)
+    hostname = db.Column(db.String(100))
+    
+    # System Information
     os_name = db.Column(db.String(100))
     os_version = db.Column(db.String(50))
-    department = db.Column(db.String(100))
+    architecture = db.Column(db.String(20))
+    
+    # Hardware Information
+    cpu_model = db.Column(db.String(100))
+    cpu_cores = db.Column(db.Integer)
+    ram_total = db.Column(db.String(20))
+    storage_info = db.Column(db.Text)
+    gpu_info = db.Column(db.Text)
+    
+    # Network Information
+    subnet = db.Column(db.String(20))
+    vlan_id = db.Column(db.Integer)
+    switch_port = db.Column(db.String(50))
+    
+    # Administrative Information
     inventory_number = db.Column(db.String(50))
-    serial_number = db.Column(db.String(100))
-    model = db.Column(db.String(100))
-    manufacturer = db.Column(db.String(100))
-    location = db.Column(db.String(255))
+    location = db.Column(db.String(100))
+    department = db.Column(db.String(100))
     responsible_person = db.Column(db.String(100))
-    description = db.Column(db.Text)
-    is_active = db.Column(db.Boolean, default=True)
+    warranty_until = db.Column(db.Date)
+    
+    # Timestamps
     first_discovered = db.Column(db.DateTime, default=datetime.utcnow)
     last_seen = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    deleted_at = db.Column(db.DateTime)
+    is_active = db.Column(db.Boolean, default=True)
     
     def __repr__(self):
         return f'<Equipment {self.ip_address} - {self.hostname}>'
 
-# ScanHistory остается без изменений
 class ScanHistory(db.Model):
     __tablename__ = 'scan_history'
+    
     id = db.Column(db.Integer, primary_key=True)
-    subnet_scanned = db.Column(db.String(50), nullable=False)
-    devices_found = db.Column(db.Integer, default=0)
+    subnet_scanned = db.Column(db.String(20), nullable=False)
+    devices_found = db.Column(db.Integer)
+    scan_type = db.Column(db.String(20))
     scan_date = db.Column(db.DateTime, default=datetime.utcnow)
-    scan_type = db.Column(db.String(20))  # manual, scheduled, quick
     initiated_by = db.Column(db.String(100))
-    vlan_id = db.Column(db.Integer)
     
     def __repr__(self):
         return f'<ScanHistory {self.subnet_scanned} - {self.scan_date}>'
